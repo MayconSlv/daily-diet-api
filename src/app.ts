b9e5b4da073eef72e561e3dab48ee1e3a710924e
Maycon Silva <mayconslv.contato@gmail.com>
2023-08-20 14:36:42 -0300

    chore: register routes and features

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import fastify from 'fastify'
+import jwt from '@fastify/jwt'
 import { authRoutes } from './routes/auth'
+import { mealRoutes } from './routes/meals'
 
 const app = fastify()
 
+app.register(jwt, {
+  secret: 'QICK19JKK1Z032JANAEQRIMDAFS9',
+})
 app.register(authRoutes, {
   prefix: 'auth',
+})
+app.register(mealRoutes, {
+  prefix: 'meal',
 })
 
 app
